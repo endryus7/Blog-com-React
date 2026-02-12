3bc09dfa3639f0e4b15d238c2bd87966147d82c8
Endryus-Montezano <endryusmontezano@live.com>
2026-02-12 18:08:37 -0300

    Configurando o React Router

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+
 function App() {
 
 
   return (
     <div className='App'>
-      <h1>React Axios</h1>
+      <div className='container'>
+        <Outlet />
+      </div>
     </div>
   )
 }
