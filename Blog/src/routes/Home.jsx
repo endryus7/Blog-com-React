5aa482bbfdb8e5da55bb7304a378f3772adcc00d
Endryus-Montezano <endryusmontezano@live.com>
2026-02-12 20:21:15 -0300

    Resgata dados da API

--- Blog/src/routes/Home.jsx
+++ Blog/src/routes/Home.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { useState, useEffect, use } from "react"
+
+import { Link } from "react-router-dom"
+
+import axios from "axios"
+
+import "./Home.css"
 
 const Home = () => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async() => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+            const data = response.data
+
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
   return (
     <div>
         <h1>Home</h1>
