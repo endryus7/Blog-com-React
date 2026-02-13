c4c70165852a095b2f5c970dad2e82f5c34f906d
Endryus-Montezano <endryusmontezano@live.com>
2026-02-13 18:46:57 -0300

    Cria edição do post

--- Blog/src/routes/EditPost.jsx
+++ Blog/src/routes/EditPost.jsx
@@ -10,7 +10,35 @@ const EditPost = () => {
     const [title, setTitle] = useState()
     const [body, setBody] = useState()
 
-    const {id} = useParams
+    const {id} = useParams()
+
+    const getPost = async() => {
+        try {
+            const response = await blogFetch.get(`/posts/${id}`)
+            
+            const data = response.data
+
+            setTitle(data.title)
+            setBody(data.body)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const editPost = async(e) => {
+        e.preventDefault()
+
+        const post = {title, body, userId: 1}
+
+        await blogFetch.put(`/posts/${id}`, {
+            body: post,
+        })
+    }
+
+    useEffect(() => {
+        getPost()
+    }, [])
 
   return (
     <div className='new-post'>
@@ -23,6 +51,7 @@ const EditPost = () => {
             name='title' 
             placeholder='Digite o título'
             onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
             />
           </div>
           <div className="form-control">
@@ -32,6 +61,7 @@ const EditPost = () => {
             id="body" 
             placeholder='Digite o conteúdo'
             onChange={(e) => setBody(e.target.value)}
+            value={body || ""}
             ></textarea>
           </div>
           <input type="submit" value="Editar Post" className='btn'/>
